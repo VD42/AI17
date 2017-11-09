6f1bd14a124106d2657956ae1eccc78835f31573
Vladislav Kochemaev <kochemaev@indigobyte.com>
2017-11-09 13:34:39 +0400

    [12] change build structure

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -44,11 +44,14 @@ void MyStrategy::move(model::Player const& me, model::World const& world, model:
 {
 	static std::vector<VehicleRealInfo> vehicles;
 	static bool rush = false;
+	static std::pair<double, double> arrv_pos;
 
 	if (world.getTickIndex() == 0)
 	{
 		for (auto const& v : world.getNewVehicles())
 			vehicles.push_back({ v.getId(), v.getPlayerId(), v.getType(), v.getX(), v.getY(), v.getDurability() });
+
+		arrv_pos = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_ARRV);
 	}
 
 	for (auto const& u : world.getVehicleUpdates())
@@ -59,6 +62,49 @@ void MyStrategy::move(model::Player const& me, model::World const& world, model:
 				v.y = u.getY();
 				v.durability = u.getDurability();
 			}
+
+
+	if (world.getTickIndex() < 200)
+	{
+		if (world.getTickIndex() == 0)
+		{
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(0.0);
+			move.setRight(0.0);
+			move.setRight(game.getWorldWidth());
+			move.setBottom(game.getWorldHeight());
+		}
+		else if (world.getTickIndex() == 1)
+		{
+			move.setAction(model::ActionType::ACTION_MOVE);
+			move.setX(128.0 + 32.0 - arrv_pos.first);
+			move.setY(128.0 + 32.0 - arrv_pos.second);
+			move.setMaxSpeed(game.getTankSpeed() * 0.6);
+		}
+		if (world.getTickIndex() == 199)
+			arrv_pos = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_ARRV);
+		return;
+	}
+
+	if (world.getTickIndex() < 300)
+	{
+		if (world.getTickIndex() == 200)
+		{
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(0.0);
+			move.setRight(0.0);
+			move.setRight(game.getWorldWidth());
+			move.setBottom(game.getWorldHeight());
+		}
+		else if (world.getTickIndex() == 201)
+		{
+			move.setAction(model::ActionType::ACTION_ROTATE);
+			move.setX(arrv_pos.first);
+			move.setY(arrv_pos.second);
+			move.setAngle(PI);
+		}
+		return;
+	}
 
 	if (world.getTickIndex() == 10000 && me.getScore() == 0)
 		rush = true;
@@ -74,8 +120,8 @@ void MyStrategy::move(model::Player const& me, model::World const& world, model:
 		else if (world.getTickIndex() == 10062)
 		{
 			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(game.getWorldWidth());
-			move.setY(game.getWorldHeight());
+			move.setX(game.getWorldWidth() - 64.0 - arrv_pos.first);
+			move.setY(game.getWorldHeight() - 64.0 - arrv_pos.second);
 			move.setMaxSpeed(game.getTankSpeed() * 0.6);
 		}
 		return;
@@ -83,21 +129,44 @@ void MyStrategy::move(model::Player const& me, model::World const& world, model:
 
 	if ((world.getTickIndex() % 20) == 0)
 	{
-		move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-		move.setRight(game.getWorldWidth());
-		move.setBottom(game.getWorldHeight());
-		switch (((world.getTickIndex() / 20) % 10))
+		switch (((world.getTickIndex() / 20 + 4) % 6))
 		{
-		case 0: move.setVehicleType(model::VehicleType::VEHICLE_IFV); break;
-		case 1: move.setVehicleType(model::VehicleType::VEHICLE_TANK); break;
-		case 2: move.setVehicleType(model::VehicleType::VEHICLE_ARRV); break;
-		case 3: move.setVehicleType(model::VehicleType::VEHICLE_HELICOPTER); break;
-		case 4: move.setVehicleType(model::VehicleType::VEHICLE_FIGHTER); break;
-		case 5: break;
-		case 6:
-		case 7:
-		case 8:
-		case 9:
+		case 0:
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(0.0);
+			move.setTop(0.0);
+			move.setRight(arrv_pos.first);
+			move.setBottom(arrv_pos.second);
+			break;
+		case 1:
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(0.0);
+			move.setTop(arrv_pos.second);
+			move.setRight(arrv_pos.first);
+			move.setBottom(game.getWorldHeight());
+			break;
+		case 2:
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(arrv_pos.first);
+			move.setTop(0.0);
+			move.setRight(game.getWorldWidth());
+			move.setBottom(arrv_pos.second);
+			break;
+		case 3:
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(arrv_pos.first);
+			move.setTop(arrv_pos.second);
+			move.setRight(game.getWorldWidth());
+			move.setBottom(game.getWorldHeight());
+			break;
+		case 4:
+			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
+			move.setLeft(0.0);
+			move.setTop(0.0);
+			move.setRight(game.getWorldWidth());
+			move.setBottom(game.getWorldHeight());
+			break;
+		case 5:
 			move.setAction(model::ActionType::ACTION_NONE);
 			break;
 		}
@@ -116,48 +185,35 @@ void MyStrategy::move(model::Player const& me, model::World const& world, model:
 
 	if ((world.getTickIndex() % 20) == 1)
 	{
-		move.setAction(model::ActionType::ACTION_MOVE);
-		std::pair<double, double> tmp;
-		switch (((world.getTickIndex() / 20) % 10))
+		switch (((world.getTickIndex() / 20 + 4) % 6))
 		{
 		case 0:
-			tmp = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_IFV);
-			move.setX(64.0 + 32.0 - tmp.first);
-			move.setY(64.0 + 32.0 - tmp.second);
+			move.setAction(model::ActionType::ACTION_MOVE);
+			move.setX(64.0);
+			move.setY(64.0);
 			break;
 		case 1:
-			tmp = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_TANK);
-			move.setX(64.0 + 32.0 - tmp.first);
-			move.setY(64.0 + 32.0 - tmp.second);
+			move.setAction(model::ActionType::ACTION_MOVE);
+			move.setX(64.0);
+			move.setY(-64.0);
 			break;
 		case 2: 
-			tmp = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_ARRV);
-			move.setX(64.0 + 32.0 - tmp.first);
-			move.setY(64.0 + 32.0 - tmp.second);
+			move.setAction(model::ActionType::ACTION_MOVE);
+			move.setX(-64.0);
+			move.setY(64.0);
 			break;
 		case 3: 
-			tmp = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_HELICOPTER);
-			move.setX(64.0 + 32.0 - tmp.first);
-			move.setY(64.0 + 32.0 - tmp.second);
+			move.setAction(model::ActionType::ACTION_MOVE);
+			move.setX(-64.0);
+			move.setY(-64.0);
 			break;
 		case 4:
-			tmp = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_FIGHTER);
-			move.setX(64.0 + 32.0 - tmp.first);
-			move.setY(64.0 + 32.0 - tmp.second);
+			move.setAction(model::ActionType::ACTION_ROTATE);
+			move.setX(arrv_pos.first);
+			move.setY(arrv_pos.second);
+			move.setAngle(PI);
 			break;
 		case 5:
-			move.setAction(model::ActionType::ACTION_ROTATE);
-			move.setX(64.0 + 32.0);
-			move.setY(64.0 + 32.0);
-			if (rand() % 2 == 0)
-				move.setAngle(PI);
-			else
-				move.setAngle(-PI);
-			break;
-		case 6:
-		case 7:
-		case 8:
-		case 9:
 			move.setAction(model::ActionType::ACTION_NONE);
 			break;
 		}
