219e33eedd2470703f491109e1844e4b2eeac45f
Vladislav Kochemaev <kochemaev@indigobyte.com>
2017-11-13 13:35:22 +0400

    Fresh start

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -6,33 +6,29 @@
 #include <cmath>
 #include <cstdlib>
 
-struct VehicleRealInfo
+class CMove : public model::Move
 {
-	long long id;
-	long long pid;
-	model::VehicleType type;
-	double x;
-	double y;
-	int durability;
+public:
+	int m_wait;
+	CMove() : model::Move(), m_wait(0) {}
 };
 
-
-std::pair<double, double> GetCenter(int pid, std::vector<VehicleRealInfo> const& vehicles, model::VehicleType type)
+std::pair<double, double> GetCenter(int pid, std::vector<model::Vehicle> const& vehicles, model::VehicleType type)
 {
 	int count = 0;
 	std::pair<double, double> result;
 
 	for (auto const& v : vehicles)
 	{
-		if (v.pid != pid)
+		if (v.getPlayerId() != pid)
 			continue;
-		if (v.durability == 0)
+		if (v.getDurability() == 0)
 			continue;
-		if (v.type == type)
+		if (v.getType() == type)
 		{
 			count++;
-			result.first += v.x * v.x;
-			result.second += v.y * v.y;
+			result.first += v.getX() * v.getX();
+			result.second += v.getY() * v.getY();
 		}
 	}
 	if (count == 0)
@@ -40,186 +36,61 @@ std::pair<double, double> GetCenter(int pid, std::vector<VehicleRealInfo> const&
 	return { sqrt(result.first / (double)count), sqrt(result.second / (double)count) };
 }
 
-void MyStrategy::move(model::Player const& me, model::World const& world, model::Game const& game, model::Move & move)
+void MyStrategy::move(model::Player const& me, model::World const& world, model::Game const& game, model::Move & new_move)
 {
-	static std::vector<VehicleRealInfo> vehicles;
-	static bool rush = false;
-	static std::pair<double, double> arrv_pos;
+	static std::vector<model::Vehicle> vehicles;
+	static std::vector<CMove> moves = { CMove() };
+	static int current_move = 1;
+	static int last_move_time = 0;
 
-	if (world.getTickIndex() == 0)
-	{
-		for (auto const& v : world.getNewVehicles())
-			vehicles.push_back({ v.getId(), v.getPlayerId(), v.getType(), v.getX(), v.getY(), v.getDurability() });
+	// update vehicles list
 
-		arrv_pos = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_ARRV);
-	}
+	for (auto const& v : world.getNewVehicles())
+		vehicles.push_back(v);
 
 	for (auto const& u : world.getVehicleUpdates())
 		for (auto & v : vehicles)
-			if (u.getId() == v.id)
-			{
-				v.x = u.getX();
-				v.y = u.getY();
-				v.durability = u.getDurability();
-			}
+			if (u.getId() == v.getId())
+				v = model::Vehicle(v, u);
 
+	// get move
 
-	if (world.getTickIndex() < 200)
+	if (last_move_time + moves[current_move - 1].m_wait >= world.getTickIndex())
 	{
-		if (world.getTickIndex() == 0)
+		if ((int)moves.size() > current_move && me.getRemainingActionCooldownTicks() == 0)
 		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(0.0);
-			move.setRight(0.0);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(game.getWorldHeight());
-		}
-		else if (world.getTickIndex() == 1)
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(128.0 + 32.0 - arrv_pos.first);
-			move.setY(128.0 + 32.0 - arrv_pos.second);
-			move.setMaxSpeed(game.getTankSpeed() * 0.6);
-		}
-		if (world.getTickIndex() == 199)
-			arrv_pos = GetCenter(me.getId(), vehicles, model::VehicleType::VEHICLE_ARRV);
-		return;
-	}
-
-	if (world.getTickIndex() < 360)
-	{
-		if (world.getTickIndex() == 200)
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(0.0);
-			move.setRight(0.0);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(game.getWorldHeight());
-		}
-		else if (world.getTickIndex() == 201)
-		{
-			move.setAction(model::ActionType::ACTION_ROTATE);
-			move.setX(arrv_pos.first);
-			move.setY(arrv_pos.second);
-			move.setAngle(PI);
-		}
-		return;
-	}
-
-	/*
-	if (world.getTickIndex() == 10000 && me.getScore() == 0)
-		rush = true;
-
-	if (rush)
-	{
-		if (world.getTickIndex() == 10061)
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(game.getWorldHeight());
-		}
-		else if (world.getTickIndex() == 10062)
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(game.getWorldWidth() - 64.0 - arrv_pos.first);
-			move.setY(game.getWorldHeight() - 64.0 - arrv_pos.second);
-			move.setMaxSpeed(game.getTankSpeed() * 0.6);
-		}
-		return;
-	}
-	*/
-
-	if (world.getTickIndex() % 60 == 0)
-	{
-		if ((world.getTickIndex() / 60) % 2 == 0)
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(0.0);
-			move.setTop(0.0);
-			move.setRight(arrv_pos.first - 32.0);
-			move.setBottom(game.getWorldHeight());
-		}
-		else
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(0.0);
-			move.setTop(0.0);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(arrv_pos.second - 32.0);
-		}
-	}
-	
-	if (world.getTickIndex() % 60 == 1)
-	{
-		if ((world.getTickIndex() / 60) % 2 == 0)
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(64.0);
-			move.setY(0.0);
-		}
-		else
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(0.0);
-			move.setY(64.0);
+			auto const& mv = moves[current_move];
+			new_move.setAction(mv.getAction());
+			new_move.setAngle(mv.getAngle());
+			new_move.setBottom(mv.getBottom());
+			new_move.setFacilityId(mv.getFacilityId());
+			new_move.setGroup(mv.getGroup());
+			new_move.setLeft(mv.getLeft());
+			new_move.setMaxAngularSpeed(mv.getMaxAngularSpeed());
+			new_move.setMaxSpeed(mv.getMaxSpeed());
+			new_move.setRight(mv.getRight());
+			new_move.setTop(mv.getTop());
+			new_move.setVehicleType(mv.getVehicleType());
+			new_move.setX(mv.getX());
+			new_move.setY(mv.getY());
+			
+			current_move++;
+			last_move_time = world.getTickIndex();
 		}
 	}
 
-	if (world.getTickIndex() % 60 == 2)
-	{
-		if ((world.getTickIndex() / 60) % 2 == 0)
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(arrv_pos.first + 32.0);
-			move.setTop(0.0);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(game.getWorldHeight());
-		}
-		else
-		{
-			move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-			move.setLeft(0.0);
-			move.setTop(arrv_pos.second + 32.0);
-			move.setRight(game.getWorldWidth());
-			move.setBottom(game.getWorldHeight());
-		}
-	}
+	CMove move;
 
-	if (world.getTickIndex() % 60 == 3)
-	{
-		if ((world.getTickIndex() / 60) % 2 == 0)
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(-64.0);
-			move.setY(0.0);
-		}
-		else
-		{
-			move.setAction(model::ActionType::ACTION_MOVE);
-			move.setX(0.0);
-			move.setY(-64.0);
-		}
-	}
+	// begin of strategy
 
-	if (world.getTickIndex() % 60 == 40)
-	{
-		move.setAction(model::ActionType::ACTION_CLEAR_AND_SELECT);
-		move.setLeft(0.0);
-		move.setTop(0.0);
-		move.setRight(game.getWorldWidth());
-		move.setBottom(game.getWorldHeight());
-	}
 
-	if (world.getTickIndex() % 60 == 41)
-	{
-		move.setAction(model::ActionType::ACTION_ROTATE);
-		move.setX(arrv_pos.first);
-		move.setY(arrv_pos.second);
-		//if ((world.getTickIndex() / 120) % 2 == 0)
-			move.setAngle(PI);
-		//else
-		//	move.setAngle(-PI);
-	}
+
+	// end of strategy
+
+	// save move
+
+	if (move.getAction() != model::ActionType::ACTION_NONE)
+		moves.push_back(move);
 }
 
 MyStrategy::MyStrategy()
